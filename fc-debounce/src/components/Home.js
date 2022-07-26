de2dda5b6035634bd5f1b807758a465b44b1a797
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-07-25 22:48:26 -0300

    Update Fetch
    Implementação do Fetch com o Axios e criação do Search

--- fc-debounce/src/components/Home.js
+++ fc-debounce/src/components/Home.js
@@ -2,16 +2,28 @@ import React,{useState,useEffect} from "react";
 import axios from "axios";
 
 const Home =()=>{
+    const [search,setSearch] = useState("")
     const [dados,setdados] = useState([])
+    const params =
+    
     useEffect(()=>{
-        axios.get("http://localhost:5000/Clients")
+      axios.get("http://localhost:5000/Clients",{
+        params
+      })
         .then((resp)=>{
             setdados(resp.data)
         })
     },[])
-    
-
-return<>Heloo resp fetching</>
-
+return (
+    <>
+    <input type="search" placeholder="Faça sua buscar" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+    <ul>{
+            dados.map((client)=>(
+                <li key={client.userID}>{client.userName}</li>
+            ))
+        }
+    </ul>
+    </>
+)
 }
 export default Home
