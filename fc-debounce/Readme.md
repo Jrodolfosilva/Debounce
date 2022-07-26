3efde86e736d6161c7f081d96a3fe32df23714bb
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-07-25 23:35:29 -0300

    Update Debounce
    Implementação do Debounce

--- fc-debounce/Readme.md
+++ fc-debounce/Readme.md
@@ -4,12 +4,14 @@
 
 <p align="center">
   <a href="#-sobre">Sobre</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+   <a href="#-Tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+
 </p>
 
 
 ## 📚 Sobre
 
-<p align="justify">Repositório dedicado a resoluções de um dos problemas ao implementar um input type="search" com requisi�oes *OverFetch* </p>
+<p align="justify">Repositório dedicado a resolução de um dos problemas ao implementar um input type="search" que realiza requisi�oes diretamento no servidor(API json-server). A solução encontrada é utilizar o setTimeOut como agendador e o clearTimeOut para cancelar a operação quando houver um novo agendamento. Sem o Debounce, toda tecla digitada geral uma nova requisição e com o Debounce, cancelamos esses agendamentos caso outra letra seja digitada em um intervalo menor ou igual a 1segundo.</p>
                                   
 
 ## 🚀 Tecnologias
@@ -17,7 +19,11 @@
 O projeto foi desenvolvido a partir das tecnologias:
 
 -*JAVASCRIPT*
-- *REACT JS*
+- *REACT JS* {
+  useState,
+  useEffect,
+  useRef
+}
 
 
 --------------
--- fc-debounce/src/components/Home.js
+++ fc-debounce/src/components/Home.js
@@ -1,26 +1,48 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import axios from "axios";
 
 const Home =()=>{
+    const timeoutRef = useRef()
     const [search,setSearch] = useState("")
     const [dados,setdados] = useState([])
-    const params =
-    
+
+    const Debounce = (func)=>{
+
+        clearTimeout(timeoutRef.current)
+     
+            timeoutRef.current = setTimeout(()=>{
+               return func()
+            },1000)
+
+    }
     useEffect(()=>{
-      axios.get("http://localhost:5000/Clients",{
-        params
-      })
-        .then((resp)=>{
-            setdados(resp.data)
-        })
-    },[])
+
+        const params={}
+        if(search){
+            params.userName_like = search
+        }
+
+        Debounce(
+            function HttpGet (){
+                axios.get("http://localhost:5000/Clients",{params})
+                .then((resp)=>{
+                    setdados(resp.data)
+                })
+                
+            }
+          
+
+        )
+        
+    },[search])
 return (
     <>
     <input type="search" placeholder="Faça sua buscar" value={search} onChange={(e)=>setSearch(e.target.value)}/>
     <ul>{
+        dados.length?
             dados.map((client)=>(
                 <li key={client.userID}>{client.userName}</li>
-            ))
+            )):<p>Carregando...</p>
         }
     </ul>
     </>
