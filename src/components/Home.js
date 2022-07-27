21eecb49b77652e1aa095e84e14d8f49b8ce62be
Jrodolfosilva <100287751+Jrodolfosilva@users.noreply.github.com>
2022-07-26 23:22:33 -0300

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,17 @@ import React,{useState,useEffect,useRef} from "react";
 import axios from "axios";
 
 const Home =()=>{
-    const timeoutRef = useRef()
     const [search,setSearch] = useState("")
     const [dados,setdados] = useState([])
-
+    const timeoutRef = useRef()
     const Debounce = (func)=>{
-
-        clearTimeout(timeoutRef.current)
-     
+        clearTimeout(timeoutRef.current)  
             timeoutRef.current = setTimeout(()=>{
                return func()
             },1000)
-
     }
-    useEffect(()=>{
 
+    useEffect(()=>{
         const params={}
         if(search){
             params.userName_like = search
@@ -27,22 +23,26 @@ const Home =()=>{
                 axios.get("http://localhost:5000/Clients",{params})
                 .then((resp)=>{
                     setdados(resp.data)
-                })
-                
+                })  
             }
-          
-
         )
         
     },[search])
-return (
+
+    return (
     <>
-    <input type="search" placeholder="Faça sua buscar" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-    <ul>{
+    <input
+     type="search"
+     placeholder="Faça sua buscar"
+     value={search}
+     onChange={(e)=>setSearch(e.target.value)}/>
+    <ul>
+        {
         dados.length?
-            dados.map((client)=>(
-                <li key={client.userID}>{client.userName}</li>
-            )):<p>Carregando...</p>
+        dados.map((client)=>(
+        <li key={client.userID}>{client.userName}</li>
+        ))
+        :<p>Carregando...</p>
         }
     </ul>
     </>
